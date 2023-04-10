676009476e0785eb8d406e1e49dd67f590406c9f
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-11 00:16:05 +0200

    Make dotnet format succeed in style mode

--- src/Ryujinx.Ava/UI/Models/SaveModel.cs
+++ src/Ryujinx.Ava/UI/Models/SaveModel.cs
@@ -57,7 +57,7 @@ namespace Ryujinx.Ava.UI.Models
             return "0 KiB";
         }
 
-        public SaveModel(SaveDataInfo info, VirtualFileSystem virtualFileSystem)
+        public SaveModel(SaveDataInfo info)
         {
             SaveId = info.SaveDataId;
             TitleId = info.ProgramId;
