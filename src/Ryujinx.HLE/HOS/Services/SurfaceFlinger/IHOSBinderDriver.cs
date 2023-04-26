c36a367c6babf56f0b11b25b015226a094298163
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-04-26 05:33:27 +0200

    Run dotnet format analyzers after rebase

--- src/Ryujinx.HLE/HOS/Services/SurfaceFlinger/IHOSBinderDriver.cs
+++ src/Ryujinx.HLE/HOS/Services/SurfaceFlinger/IHOSBinderDriver.cs
@@ -85,7 +85,7 @@ namespace Ryujinx.HLE.HOS.Services.SurfaceFlinger
 
             ReadOnlySpan<byte> inputParcel = context.Memory.GetSpan(dataPos, (int)dataSize);
 
-            using IMemoryOwner<byte> outputParcelOwner = ByteMemoryPool.Shared.RentCleared(replySize);
+            using IMemoryOwner<byte> outputParcelOwner = ByteMemoryPool.RentCleared(replySize);
             Span<byte> outputParcel = outputParcelOwner.Memory.Span;
 
             ResultCode result = OnTransact(binderId, code, flags, inputParcel, outputParcel);
